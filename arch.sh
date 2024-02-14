eed9d7bcafc1c276463b726d8bb2433313c0352c
ToM_There <24317684+Tom-there@users.noreply.github.com>
2024-02-14 15:21:36 +0100

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# my autoexec is inspired by Luke's Auto Rice Boostrapping Script (LARBS)
+# by Tom Schreiner <tom.schreiner@gmx.de> (orig. Luke Smith <luke@lukesmith.xyz>)
+# License: TBD
+
+### OPTIONS AND VARIABLES ###
+
+dotfilesrepo="https://github.com/Tom-there/.files.git"
+aurhelper="yay"
+
+echo "test"
+
+
