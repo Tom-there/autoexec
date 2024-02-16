0977967a3b06673130c6ea4bc2bd4e52eb8e075a
ToM_There <24317684+Tom-there@users.noreply.github.com>
2024-02-16 20:07:13 +0100

    Update autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -1,50 +1,5 @@
 #!/bin/sh
-# initial autoexec that creates user account and downloads correct user specific autoexec file
+# autoexec that configures my user from scratch
 # by Tom Schreiner <tom.schreiner@gmx.de>
 
-installpkg() {
-	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
-}
-
-getuserinfo(){
-	name=$(whiptail --inputbox "Welcome!\n First, please enter your preferred username." 10 60 3>&1 1>&2 2>&3 3>&1)
-	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
-		name=$(whiptail --nocancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done
-	pass1=$(whiptail --nocancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	while ! [ "$pass1" = "$pass2" ]; do
-		unset pass2
-		pass1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done
-}
-
-adduser(){
-	whiptail --infobox "Adding user \"$name\"..." 10 60
-	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-	usermod -a -G wheel "$name"
- 	mkdir -p /home/"$name"
-  	chown "$name":wheel /home/"$name"
-	export repodir="/home/$name/.local/src"
-	mkdir -p "$repodir"
-	chown -R "$name":wheel "$(dirname "$repodir")"
-	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2
-}
-
-handleuser(){
-	getuserinfo
- 	adduser
-}
-
-pacman --noconfirm --needed -Syu 
-installpkg libnewt
-handleuser
-
-
-for x in curl base-devel git ntp vim zsh; do
-	whiptail --title "needed package installation" \
-		--infobox "Installing \`$x\`." 10 60
-	installpkg "$x"
-done
+echo "test"
