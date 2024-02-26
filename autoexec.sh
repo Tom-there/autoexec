c5600db21706b9460425c1c34608ddd970a2e361
Tom Schreiner <tom.schreiner@gmx.de>
2024-02-26 14:07:16 +0100

    migrating to dialog

--- autoexec.sh
+++ autoexec.sh
@@ -27,12 +27,12 @@ usererror(){
 # 1 init
 #   Gets the system ready for user creation
 # 1.1 check if this is arch
-pacman --noconfirm --needed -Sy libnewt || error "Are you sure this is an arch distro and you are logged in as root? If yes, check your internet connection"
+pacman --noconfirm --needed -Sy dialog || error "Are you sure this is an arch distro and you are logged in as root? If yes, check your internet connection"
 
 # 1.2 welcome
 welcome(){
-  whiptail --title "Welcome!" --msgbox "This script is designed to install everything you need to be productive!" 10 50
-  whiptail --title "Welcome!" --yes-button "Lets go!" --no-button "wait..." --yesno "Are you ready to set up your user?" 10 50
+  dialog --title "Welcome!" --msgbox "This script is designed to install everything you need to be productive!" 10 50
+  dialog --title "Welcome!" --yes-button "Lets go!" --no-button "wait..." --yesno "Are you ready to set up your user?" 10 50
 }
 welcome || usererror
 
@@ -43,14 +43,14 @@ sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
 
 # 1.4 refreshing arch keyring
 refreshing(){
-  whiptail --title "installing..." --infobox "Refreshing arch keyring" 10 50
+  dialog --title "installing..." --infobox "Refreshing arch keyring" 10 50
   pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
 }
 refreshing
 
 # 1.5 installing base packages
 for x in curl ca-certificates base-devel git ntp zsh; do
-  whiptail --title "installing..." --infobox "Installing $x" 10 50
+  dialog --title "installing..." --infobox "Installing $x" 10 50
   installpkg "$x"
 done
 
@@ -61,21 +61,21 @@ ntpd -q -g >/dev/null
 #   created the new user and sets it up
 # 2.1 ask for username
 getusername(){
-  username=$(whiptail --inputbox "First, enter your desired username." 10 50 3>&1 1>&2 2>&3 3>&1)
+  username=$(dialog --inputbox "First, enter your desired username." 10 50 3>&1 1>&2 2>&3 3>&1)
   while ! echo "$username" | grep -q "^[a-z_][a-z0-9_]*$"; do
-    username=$(whiptail --nocancel --inputbox "Username not valid.\nPlease enter valid username that matches RegEx: '[a-z_][a-z0-9_]*'" 10 50 3>&1 1>&2 2>&3 3>&1)
+    username=$(dialog --nocancel --inputbox "Username not valid.\nPlease enter valid username that matches RegEx: '[a-z_][a-z0-9_]*'" 10 50 3>&1 1>&2 2>&3 3>&1)
   done
 }
 getusername || usererror
 
 # 2.2 ask for password
 getpassword(){
-  userpass1=$(whiptail --nocancel --passwordbox "What would you like as your password?" 10 50 3>&1 1>&2 2>&3 3>&1)
-  userpass2=$(whiptail --nocancel --passwordbox "Retype password." 10 50 3>&1 1>&2 2>&3 3>&1)
+  userpass1=$(dialog --nocancel --passwordbox "What would you like as your password?" 10 50 3>&1 1>&2 2>&3 3>&1)
+  userpass2=$(dialog --nocancel --passwordbox "Retype password." 10 50 3>&1 1>&2 2>&3 3>&1)
   while ! [ "$userpass1" = "$userpass2" ]; do
     unset userpass2
-    userpass1=$(whiptail --nocancel --passwordbox "Passwords did not match. Try again" 10 50 3>&1 1>&2 2>&3 3>&1)
-    userpass2=$(whiptail --nocancel --passwordbox "Retype password." 10 50 3>&1 1>&2 2>&3 3>&1)
+    userpass1=$(dialog --nocancel --passwordbox "Passwords did not match. Try again" 10 50 3>&1 1>&2 2>&3 3>&1)
+    userpass2=$(dialog --nocancel --passwordbox "Retype password." 10 50 3>&1 1>&2 2>&3 3>&1)
   done
 }
 getpassword || usererror
