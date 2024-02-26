036cd89230da7839833695b8e5b71321f39a5c71
Tom Schreiner <tom.schreiner@gmx.de>
2024-02-26 14:14:57 +0100

    fixed lock on welcome screen

--- autoexec.sh
+++ autoexec.sh
@@ -33,6 +33,7 @@ pacman --noconfirm --needed -Sy dialog || error "Are you sure this is an arch di
 welcome(){
   dialog --title "Welcome!" --msgbox "This script is designed to install everything you need to be productive!" 10 50
   dialog --title "Welcome!" --yes-button "Lets go!" --no-button "wait..." --yesno "Are you ready to set up your user?" 10 50
+  return $?
 }
 welcome || usererror
 
